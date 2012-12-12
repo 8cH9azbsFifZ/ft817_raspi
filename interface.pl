eadb7cd5b492f93ba597f7691c5db4ae0db44c98
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2012-12-13 00:15:36 +0100

    wip

--- interface.pl
+++ interface.pl
@@ -1,69 +1,76 @@
 #!/usr/bin/perl -I/opt/local/lib/perl5//vendor_perl/5.12.4/darwin-thread-multi-2level/
-
-
-$model=120;  #ft817
-$speed=38400;
-$device="/dev/tty.usbserial";
-$port=4532;
-$cmd="rigctld -vvvv --rig-file=$device --model=$model --serial-speed=$speed --port=$port";
-#system("$cmd &");
-
 use IO::Socket;
-use IO::Select;
 
-# Create a socket
-my $socket = new IO::Socket::INET (
-  PeerAddr => '127.0.0.1',
-  PeerPort => $port,
-  Proto => 'tcp',
-);
-$socket or die "no socket";
-
-sub get_rig()
+sub init_rig ()
 {
-print $socket "m\n";
-$mode=<$socket>;
-$bw=<$socket>;
-print $socket "f\n";
-$freq=<$socket>;
-print $socket "i\n";
-$split_freq=<$socket>;
-print $socket "c\n";
-$ctcss_tone=<$socket>;
-print $socket "r\n";
-$rpt_shift=<$socket>;
-#print $socket "w \0x00\0x00\0x00\0x00\0xE7\n";
-#FT817_READ_RX_STATE 0xE7
-#$level=<$socket>;
-#print $socket "get_dcd\n";
-#$sql_stat=<$socket>;
-chomp $mode;
-chomp $bw;
-chomp $freq;
-chomp $split_freq;
-chomp $ctcss_tone;
-chomp $rpt_shift;
-chomp $sql_stat;
-chomp $level;
-print "$freq $split_freq $mode $bw $ctcss_tone $rpt_shift $level\n";
+	$model=120;  #ft817
+	$speed=38400;
+	$device="/dev/tty.usbserial";
+	$port=4532;
+	$cmd="rigctld -vvvv --rig-file=$device --model=$model --serial-speed=$speed --port=$port";
+	#system("$cmd &");
 }
 
+my %rig;
+my $socket;
+sub init_rig_socket ()
+{
 
-#get_rig();
-use Gtk2;
+	# Create a socket
+	$socket = new IO::Socket::INET (
+	  PeerAddr => '127.0.0.1',
+	  PeerPort => $port,
+	  Proto => 'tcp',
+	);
+	$socket or die "no socket";
+}
 
-Gtk2->init;
+sub read_rig()
+{
+	print $socket "m\n";
+	$rig{mode}=<$socket>;
+	$rig{bw}=<$socket>;
+	print $socket "f\n";
+	$rig{freq}=<$socket>;
+	print $socket "i\n";
+	$rig{split_freq}=<$socket>;
+	print $socket "c\n";
+	$rig{ctcss_tone}=<$socket>;
+	print $socket "r\n";
+	$rig{rpt_shift}=<$socket>;
+	#print $socket "w \0x00\0x00\0x00\0x00\0xE7\n";
+	#FT817_READ_RX_STATE 0xE7
+	#$level=<$socket>;
+	#print $socket "get_dcd\n";
+	#$sql_stat=<$socket>;
+	chomp $rig{$_} foreach (keys %rig);
+}
 
-$window = Gtk2::Window->new('toplevel');
-$window->show;
-$window->signal_connect('delete-event' => sub { Gtk2->main_quit });
-my $label  = Gtk2::Label->new('Hello World!');
-$window->add($label);
-$label ->show;
+sub display_text ()
+{
+	$freq = $rig{freq} ; #in Hz
+	$mhz = int($freq / 1000000);
+	$khz = int(($freq % 1000000)/1000);
+	$hz = int($freq % 1000);
+
+	printf "%03d.%03d.%03d Hz\nMode: $rig{mode}\nChannel: $channel\nBand: $band\nLocator: $locator", $mhz, $khz, $hz;
+}
+
+sub maidenhead_to_wgs () { }
+sub wgs_to_maidenhead () { }
+sub read_gps () {}
+sub calculate_distance_wgs84() {}
+sub freq_to_channel() {}
+sub get_cur_band_name() {}
+sub set_channel() {}
+sub find_nearest_channel() {}
+sub signal_detected() {}
+sub watchdog() {}
+sub read_rotary () {}
 
 
-Gtk2->main;
-
-0;
+$rig{freq}=439325000;
+#read_rig();
+display_text();
 
 
